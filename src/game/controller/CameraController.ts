49e6522404d078fb678002b5fc6d43f7556dfebe
itaylayzer <itaylayzer@gmail.com>
2024-10-03 18:01:06 +0000

    Trails & Camera Shake - without their settings

--- src/game/controller/CameraController.ts
+++ src/game/controller/CameraController.ts
@@ -4,16 +4,7 @@ import { LocalPlayer } from "../player/LocalPlayer";
 import * as CANNON from "cannon-es";
 import { lerp } from "three/src/math/MathUtils.js";
 import { Player } from "../player/Player";
-function explodeFn(t: number) {
-    return t < Math.PI * 4
-        ? (100 * Math.sin(t + Math.PI)) / Math.pow(t + Math.PI, 3)
-        : 0;
-}
-function randomSign() {
-    if (Math.random() > 0.66) return -1;
-    if (Math.random() < 0.33) return 1;
-    return 0;
-}
+import { PerlinNoise } from "../api/PerlinNoise";
 
 export class CameraController {
     public camera: THREE.PerspectiveCamera;
@@ -22,14 +13,14 @@ export class CameraController {
     private turboMode: number;
     private pid: number | false;
     private players: Player[];
+    private perlin: PerlinNoise;
 
-    private forceRotation: THREE.Vector3;
     constructor(camera: THREE.PerspectiveCamera) {
         this.camera = camera;
         camera.rotation.y = Math.PI;
         this.time = Math.PI * 4;
         this.pid = false;
-        this.forceRotation = new THREE.Vector3();
+        this.perlin = new PerlinNoise();
         this.turboMode = 0;
         this.players = [];
     }
@@ -40,7 +31,6 @@ export class CameraController {
         if (this.players.length === 0) {
             this.players = Array.from(Player.clients.values());
         }
-
 
         const player = [localPlayer, this.players[this.pid as number]][
             +(this.pid !== false)
@@ -64,7 +54,9 @@ export class CameraController {
             Global.deltaTime * 10
         );
 
-        let vertical = [player.keyboard.vertical + player.mushroomAddon, 2][+player.rocketMode];
+        let vertical = [player.keyboard.vertical + player.mushroomAddon, 2][
+            +player.rocketMode
+        ];
         let horizontal = [player.keyboard.horizontal, 0][+player.rocketMode];
         let driftSide = [player.driftSide, 0][+player.rocketMode];
 
@@ -111,17 +103,23 @@ export class CameraController {
             (horizontal + driftSide) * (0.05 + 0.04 * this.turboMode)
         );
 
-        this.time += Global.deltaTime * 13;
-        this.camera.rotation.z += this.forceRotation.z * explodeFn(this.time);
-        this.camera.rotation.x += this.forceRotation.x * explodeFn(this.time);
-        this.camera.rotation.y += this.forceRotation.y * explodeFn(this.time);
-    }
+        const speedDotQuatewrnion = player.velocity.dot(
+            player.quaternion.vmult(new CANNON.Vec3(0, 0, 1))
+        );
 
-    shake(distance: number) {
-        this.time = 0; //0.93656;
-        this.forceRotation.z =
-            (randomSign() * Math.random()) / (distance * distance);
-        this.forceRotation.x = (randomSign() * Math.random()) / distance;
-        this.forceRotation.y = (randomSign() * Math.random()) / distance;
+        const timeMultiplier = 4;
+        const XYDivider = 300;
+
+        const perlinValue =
+            (this.perlin.get(0, (Global.elapsedTime % 2) * timeMultiplier) *
+                Math.abs(speedDotQuatewrnion)) /
+            XYDivider;
+
+        this.camera.rotateX(perlinValue);
+        this.camera.rotateY(perlinValue);
+        this.camera.fov = Math.min(90 + Math.abs(speedDotQuatewrnion) * 2, 120);
+        this.camera.updateProjectionMatrix();
+
+        this.time += Global.deltaTime * 13;
     }
 }
