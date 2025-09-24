17801c98ae66c3fc8895806a3d8f2c7bcfdb0676
itaylayzer <itaylayzer@gmail.com>
2025-09-24 16:28:25 +0300

    feat: title

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,7 +4,15 @@ import "react-toastify/dist/ReactToastify.css";
 import "react-tooltip/dist/react-tooltip.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Kart.IO</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
